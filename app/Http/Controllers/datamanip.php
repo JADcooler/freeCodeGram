9ee82bbefd3d4160ad28ecac50528fb2283634e4
jadejassajay <jadejassajay@gmail.com>
2021-10-14 16:02:21 +0530

    sadfsdf
    sdfsdfs

--- app/Http/Controllers/datamanip.php
+++ app/Http/Controllers/datamanip.php
@@ -9,6 +9,7 @@ class datamanip extends Controller
     //
     public function index(Request $request)
     {
+      echo $request->fathername;
        dd($request->all());  //to check all the datas dumped from the form
        //if your want to get single element,someName in this case
        //$someName = $request->someName;
